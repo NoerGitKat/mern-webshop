13dae5929d89d32538dfdb4e1d32b542a5360dad
Noer Paanakker <noer@lightworksweb.nl>
2020-10-19 16:18:39 +0200

    added bootstrap theme, uploaded images

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import "./bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
-import "./App.css";
-
-function App() {
-  return <></>;
+function App(): JSX.Element {
+  return (
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to MernShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
