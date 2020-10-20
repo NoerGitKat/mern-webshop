5f33a189611d0152ead4d59cb24264d1186d880a
Noer Paanakker <noer@lightworksweb.nl>
2020-10-20 09:26:54 +0200

    added dotenv

--- server/dist/server.js
+++ server/dist/server.js
@@ -5,12 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const products_router_1 = __importDefault(require("./routes/products-router"));
 const app = express_1.default();
+const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(cors_1.default());
 // Routes
 app.use("/api/products/", products_router_1.default);
-app.listen(5000, () => {
-    console.log(`The server is listening on port ${5000}!`);
+app.listen(PORT, () => {
+    console.log(`The server is listening on port ${PORT}!`);
 });
--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 import productsRouter from "./routes/products-router";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(cors());
@@ -11,6 +15,6 @@ app.use(cors());
 // Routes
 app.use("/api/products/", productsRouter);
 
-app.listen(5000, () => {
-  console.log(`The server is listening on port ${5000}!`);
+app.listen(PORT, () => {
+  console.log(`The server is listening on port ${PORT}!`);
 });
